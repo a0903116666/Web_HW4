e7e23f8ea5e1cb20a3cb752eb727580cd0f0a449
Toby <a0903116666@gmail.com>
2026-06-07 23:10:49 +0800

    chore: remove unnecessary comment from Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  // 解決 GitHub Pages 白畫面的關鍵
   base: '/Web_HW4/', 
   plugins: [
     vue(),
