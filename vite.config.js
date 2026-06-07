9ceefbdb11f689e4e4e1cc82c5e2d2495e6181bd
Toby <a0903116666@gmail.com>
2026-06-07 23:10:40 +0800

    refactor: reorganize Vite config for clarity and consistency

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
+  // 解決 GitHub Pages 白畫面的關鍵
   base: '/Web_HW4/', 
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-});
+})
